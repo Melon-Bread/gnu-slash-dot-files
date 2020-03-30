e8f4a3d762f8548a7b4d0ec103ddeb88c9945ea2
Melon Bread <rain@melonbread.dev>
2020-03-30 13:26:43 -0400

    Removed junk packages

--- .bin/EmuForce.sh
+++ .bin/EmuForce.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-yay -S pcsx2-git citra-canary-git yuzu-canary-git rpcs3-git dolphin-emu-git desmume-git retroarch-git --noconfirm --sudoloop
+yay -S pcsx2-git citra-canary-git yuzu-mainline-git rpcs3-git dolphin-emu-git retroarch-git --noconfirm --sudoloop
